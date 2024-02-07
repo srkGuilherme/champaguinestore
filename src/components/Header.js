16580841ab528379fe9bc658f012311304ae1ef8
Guilherme Araujo <guilhermearaujoleal@hotmail.com>
2024-02-06 22:04:49 -0300

    Added Search button and input, with animations.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Header() {
+  const [searchStyle, setSearchStyle] = useState("CloseSearch");
+
+  const handleSearchAnimation = () => {
+    console.log("Search clicked");
+    if (searchStyle !== "CloseSearch") {
+      setSearchStyle("CloseSearch");
+      console.log(searchStyle);
+    } else {
+      setSearchStyle("OpenSearch");
+      console.log(searchStyle);
+    }
+  };
+
   return (
     <div className="Header">
       <ul>
@@ -8,8 +21,15 @@ function Header() {
           <img className="Icon-in-Header" src="/assets/menu.png"></img>
         </li>
         <li>CHAMPAGUINE STORE</li>
-        <li>
-          <img className="Icon-in-Header" src="/assets/cart.png"></img>{" "}
+        <li className="SearchLi">
+          <span className={searchStyle}>
+            <img
+              className="Icon-in-Header"
+              src="/assets/search.png"
+              onClick={handleSearchAnimation}
+            ></img>
+            <input placeholder="Procurar produto"></input>
+          </span>
         </li>
       </ul>
     </div>
